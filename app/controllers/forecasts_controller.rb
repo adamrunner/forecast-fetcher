4e965ca44156cae84892528c61778e78e77dce91
Adam Runner <adamrunner@gmail.com>
2024-03-08 14:36:21 -0800

    add tailwindcss and styles for html

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -7,14 +7,10 @@ class ForecastsController < ApplicationController
     # TODO - Sanitize the address
     # TODO - Validate the address
     # Reject no addresses, redirect to error
-    address = params[:address]
-    if address.blank?
+    @address = params[:address]
+    if @address.blank?
       redirect_to root_path, alert: "Please enter an address"
     else
-      # Get the start_date and end_date from the form
-      start_date = params[:start_date]
-      end_date   = params[:end_date]
-
       # Get the API key from the environment
       api_key = ENV['VISUAL_CROSSING_API_KEY']
 
@@ -23,7 +19,7 @@ class ForecastsController < ApplicationController
       client = VisualCrossingWeatherClient.new(api_key: api_key)
 
       # Get the weather for the address, start_date, and end_date
-      @weather = client.get_weather(address: address, start_date: start_date, end_date: end_date)
+      @weather = client.get_weather(address: @address)
       @days    = @weather&.dig("days")
       @current_conditions = @weather&.dig("currentConditions")
       # Render the index view
