729c309112d67fb1f1eb4ad5499488ce5257987f
Adam Runner <adamrunner@gmail.com>
2024-03-08 15:17:01 -0800

    clean up unneeded comments

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -3,26 +3,20 @@ class ForecastsController < ApplicationController
   end
 
   def create
-    # Get the address from the form
-    # TODO - Sanitize the address
-    # TODO - Validate the address
-    # Reject no addresses, redirect to error
+    # Reject nil or blank addresses, redirect to error
+    # address is an ivar so it can be used in the form to pre-fill the input
     @address = params[:address]
     if @address.blank?
       redirect_to root_path, alert: "Please enter an address"
     else
-      # Get the API key from the environment
       api_key = ENV['VISUAL_CROSSING_API_KEY']
 
-      # Create a new instance of the VisualCrossingWeatherClient
-      # should this be moved into an initializer?
       client = VisualCrossingWeatherClient.new(api_key: api_key)
 
-      # Get the weather for the address, start_date, and end_date
-      @weather = client.get_weather(address: @address)
-      @days    = @weather&.dig("days")
-      @current_conditions = @weather&.dig("currentConditions")
-      # Render the index view
+      weather = client.get_weather(address: @address)
+      @days    = weather&.dig("days")
+      @current_conditions = weather&.dig("currentConditions")
+
       render :index
     end
   end
